c7b37445876d6bf4c6ee7ea568b5e853606fa1b7
JGabrielGarcelas <jgabrielgarcelas@gmail.com>
2026-02-19 11:22:30 -0300

    updated some files

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-function information_box() {
-    const alex_button = document.getElementById('alex_button');
-    const deni_button = document.getElementById('deni_button');
-    const gabriel_button = document.getElementById('gabriel_button');
-    const iza_button = document.getElementById('iza_button');
-    const luciano_button = document.getElementById('luciano_button');
-    const madu_button = document.getElementById('madu_button');
-    const maria_button = document.getElementById('maria_button');
-    const miguel_button = document.getElementById('miguel_button');
-    const mateus_button = document.getElementById('mateus_button');
+function information_box(){
 
-
-    alex_button.addEventListener(cancelIdleCallback, )
+var button = document.getElementById('buttons').addEventListener('click', information_box());
+    
+for (const buttons in button) {
+    console.log(123);
+    }
 }
